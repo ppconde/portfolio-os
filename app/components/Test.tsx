3f586f66307538eb8f9c731bc755dcca1d43e402
Pedro Conde <pepconde.1993@gmail.com>
2024-10-10 23:45:38 +0100

    update demo

--- app/components/Test.tsx
+++ app/components/Test.tsx
@@ -2,14 +2,11 @@ import type { LoaderFunction } from "@remix-run/cloudflare";
 import { json } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
 
-interface Env {
-    DB: D1Database;
-}
-
-export const loader: LoaderFunction = async ({ context }) => {
-    const env = context.cloudflare.env as Env;
-
-    const { results } = await env.DB.prepare("SELECT * FROM Customers LIMIT 5").all();
+export const loader: LoaderFunction = async ({ context, params }) => {
+    const { env } = context.cloudflare;
+    const { results } = await env.DB.prepare(
+        "SELECT * FROM Customers"
+    ).bind(params.productId).all();
     return json(results);
 };
 
