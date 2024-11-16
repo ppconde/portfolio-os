3be5cc29e2a2e2d9d78d61de5b8528a620ecfa79
Pedro Conde <pepconde.1993@gmail.com>
2024-11-16 21:38:33 +0000

    add comment

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,10 @@
 import { redirect } from "@remix-run/react";
 
+/**
+ * Not sure if this is the best approach,
+ * but I'm forced to have this index file,
+ * however it can't have any child routes'
+ */
 export function loader() {
   return redirect(" /website");
 }
