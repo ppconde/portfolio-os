0ca3ea77ef988cb297c14fc95d1767e83b1dfc7f
Pedro Conde <pepconde.1993@gmail.com>
2024-11-16 21:36:37 +0000

    add redirect to website

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,8 @@
+import { redirect } from "@remix-run/react";
+
+export function loader() {
+  return redirect(" /website");
+}
 
 export default function Index() {
   return <div>Something went wrong</div>;
