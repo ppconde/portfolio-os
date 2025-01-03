be09b9e6896d95456b0011ce3bd1c8f57a4f7639
Pedro Conde <pepconde.1993@gmail.com>
2025-01-03 13:47:43 +0000

    More experience changes

--- app/routes/website/experience.tsx
+++ app/routes/website/experience.tsx
@@ -2,41 +2,46 @@ export default function Experience() {
   return (
     <section className="space-y-6 font-mono text-sm text-gray-600">
       <article>
-        <h2 className="border-t pt-4 font-ps-sans-nouveaux text-2xl font-bold text-gray-900">
+        <h2 className="pt-4 font-ps-sans-nouveaux text-2xl font-bold text-gray-900">
           Experience
         </h2>
 
-        <h3 className="my-6 font-ps-sans-nouveaux font-bold text-gray-900">
-          Altice Labs
-        </h3>
-        <p>
-          I began my professional journey at <strong>Altice Labs</strong>, a
-          company dedicated to creating innovative solutions for the
-          telecommunications and IT sectors. During my time there, I was part of
-          the Operations Support System team, where I gained valuable experience
-          in organizing and planning XML workflows, developing product features,
-          debugging systems, and writing technical specifications. It was a
-          great opportunity to hone my problem-solving skills and collaborate on
-          impactful projects.
-        </p>
+        <article className="my-8">
+          <h3 className="my-4 font-ps-sans-nouveaux text-base font-bold text-gray-900">
+            Blip.pt - Frontend Engineer
+            <p className="mt-4 text-xs">[2020-Present] - Porto</p>
+          </h3>
+          <p>
+            I joined <strong>Blip.pt</strong>, where I deepened my technical
+            expertise and worked with a broad range of technologies, including{' '}
+            <em>
+              GraphQL, Node.js, TypeScript, PostgreSQL, React, Webpack, Jest,
+              Redux
+            </em>
+            , and <em>Docker</em>. I contributed to unit and functional testing,
+            and I planned and developed features for multiple apps catering to
+            betting brands. This role not only expanded my technical skill set
+            but also gave me the chance to work on complex systems and deliver
+            value to end-users.
+          </p>
+        </article>
 
-        <h3 className="my-6 font-ps-sans-nouveaux font-bold text-gray-900">
-          Blip.pt
-        </h3>
-        <p>
-          Afterward, I joined <strong>Blip.pt</strong>, where I deepened my
-          technical expertise and worked with a broad range of technologies,
-          including{' '}
-          <em>
-            GraphQL, Node.js, TypeScript, PostgreSQL, React, Webpack, Jest,
-            Redux
-          </em>
-          , and <em>Docker</em>. I contributed to unit and functional testing,
-          and I planned and developed features for multiple apps catering to
-          betting brands. This role not only expanded my technical skill set but
-          also gave me the chance to work on complex systems and deliver value
-          to end-users.
-        </p>
+        <article className="my-8">
+          <h3 className="my-4 font-ps-sans-nouveaux text-base font-bold text-gray-900">
+            Altice Labs - Informatics Consultant
+            <p className="mt-4 text-xs">[2018-2020] - Aveiro</p>
+          </h3>
+          <p className="mt-0">
+            I began my professional journey at <strong>Altice Labs</strong>, a
+            company dedicated to creating innovative solutions for the
+            telecommunications and IT sectors. During my time there, I was part
+            of the Operations Support System team, where I gained valuable
+            experience in organizing and planning XML workflows, developing
+            product features, debugging systems, and writing technical
+            specifications. It was a great opportunity to hone my
+            problem-solving skills and collaborate on impactful projects.
+          </p>
+        </article>
       </article>
     </section>
   );
