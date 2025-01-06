518c0ca480c3841e56da694f4964c8896840e540
Pedro Conde <pepconde.1993@gmail.com>
2025-01-06 19:12:43 +0000

    fix weird spacing with subtitle on home page

--- app/routes/website/home.tsx
+++ app/routes/website/home.tsx
@@ -3,8 +3,8 @@ import NavList from '~/components/NavList';
 export default function WebsiteHome() {
   return (
     <section className="flex h-full w-full flex-col items-center justify-center overflow-hidden bg-dots bg-[length:20px_20px] bg-[0_0_10px_10px] bg-repeat">
-      <header className="p-4 text-center text-gray-900">
-        <div className="flex items-center justify-center sm:h-[120px]">
+      <header className="flex flex-col p-4 text-center text-gray-900">
+        <div className="flex items-center justify-center">
           <h1 className="box-border min-h-3.5 rounded-xl border-b-4 border-l-2 border-r-4 border-t-2 border-gray-800 bg-white px-8 py-4 text-center font-perfect-dos-vga-win text-7xl transition-all delay-75 duration-300 ease-in-out hover:border-b-8 hover:border-r-8">
             Pedro Conde
           </h1>
