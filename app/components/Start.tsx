8c489a5a269e655cf5bcde48e625284cb8856e53
Pedro Conde <pepconde.1993@gmail.com>
2025-01-09 15:52:29 +0000

    create StartWindow component

--- app/components/Start.tsx
+++ app/components/Start.tsx
@@ -1,10 +1,8 @@
-import { useState, createRef } from 'react';
-import { useClickOutside } from '~/hooks/use-click-outside';
+import { useState } from 'react';
+import StartWindow from './StartWindow';
 
 export default function Start() {
   const [isOpen, setIsOpen] = useState(false);
-  const ref = createRef<HTMLDivElement>();
-  useClickOutside(ref, () => setIsOpen(false));
 
   return (
     <>
@@ -22,34 +20,7 @@ export default function Start() {
         />
         <p className="font-micro text-2xl">Start</p>
       </button>
-      {isOpen && (
-        <div
-          ref={ref}
-          className="border-windows absolute bottom-8 left-0 flex min-h-72 flex-row bg-windows-gray-primary"
-        >
-          <div className="from-windows-super-blue relative flex bg-gradient-to-tr to-windows-blue text-white">
-            <p className="writing-v-lr rotate-180 p-1 text-left font-mono">
-              <span className="font-bold">Ppconde</span>OS
-            </p>
-          </div>
-          <div className="flex flex-col justify-end">
-            {/** Separator */}
-            <div className="border-b border-windows-gray-secondary" />
-            <div className="border-t border-windows-white" />
-            {/** Start buttons */}
-            <button className="flex items-center p-1 pr-4 hover:bg-windows-blue hover:text-white">
-              <img
-                className="h-10 pr-1"
-                src="/assets/shut_down_cool-5.png"
-                alt="Windows logo"
-              />
-              <p className="font-mono text-sm tracking-tight">
-                Sh<span className="underline">u</span>t Down...
-              </p>
-            </button>
-          </div>
-        </div>
-      )}
+      {isOpen && <StartWindow setIsOpen={setIsOpen} />}
     </>
   );
 }
---
+++ app/components/StartWindow.tsx
@@ -0,0 +1,40 @@
+import { createRef } from 'react';
+import { useClickOutside } from '~/hooks/use-click-outside';
+
+type StartWindowProps = {
+  setIsOpen: (value: boolean) => void;
+};
+
+export default function StartWindow({ setIsOpen }: StartWindowProps) {
+  const ref = createRef<HTMLDivElement>();
+  useClickOutside(ref, () => setIsOpen(false));
+
+  return (
+    <div
+      ref={ref}
+      className="border-windows absolute bottom-8 left-0 flex min-h-72 flex-row bg-windows-gray-primary"
+    >
+      <div className="relative flex bg-gradient-to-tr from-windows-super-blue to-windows-blue text-white">
+        <p className="writing-v-lr rotate-180 p-1 text-left font-mono">
+          <span className="font-bold">Ppconde</span>OS
+        </p>
+      </div>
+      <div className="flex flex-col justify-end">
+        {/** Separator */}
+        <div className="border-b border-windows-gray-secondary" />
+        <div className="border-t border-windows-white" />
+        {/** Start buttons */}
+        <button className="flex items-center p-1 pr-4 hover:bg-windows-blue hover:text-white">
+          <img
+            className="h-10 pr-1"
+            src="/assets/shut_down_cool-5.png"
+            alt="Windows logo"
+          />
+          <p className="font-mono text-sm tracking-tight">
+            Sh<span className="underline">u</span>t Down...
+          </p>
+        </button>
+      </div>
+    </div>
+  );
+}
