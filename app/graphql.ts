fc49b7c15f47b462e174a547b402348a09aa3a43
Pedro Conde <pepconde.1993@gmail.com>
2025-06-05 23:50:16 +0100

    rollback to vite key

--- app/graphql.ts
+++ app/graphql.ts
@@ -3,7 +3,7 @@ import { Graffle } from 'graffle';
 export const graffle = Graffle.create().transport({
   url: 'https://api.github.com/graphql',
   headers: {
-    authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+    authorization: `Bearer ${import.meta.env.VITE_GITHUB_KEY}`,
     'User-Agent': 'portfolio-os (https://os.ppconde.com/)',
   },
 });
