52daffdc78d29239125ed9de6368abdd6b4fb63e
Pedro Conde <pepconde.1993@gmail.com>
2025-06-05 23:29:16 +0100

    changed token

--- app/graphql.ts
+++ app/graphql.ts
@@ -3,7 +3,7 @@ import { Graffle } from 'graffle';
 export const graffle = Graffle.create().transport({
   url: 'https://api.github.com/graphql',
   headers: {
-    authorization: `Bearer ${import.meta.env.VITE_GITHUB_KEY}`,
+    authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
     'User-Agent': 'portfolio-os (https://os.ppconde.com/)',
   },
 });
