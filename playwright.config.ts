934e8fb156232253caa8b1b42863b8d54e4dfbe0
Ppconde <pepconde.1993@gmail.com>
2026-02-23 23:46:54 +0000

    fix: update Playwright configuration for CI and local environments

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['blob'], ['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -84,9 +84,16 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'pnpm run dev',
-    url: 'http://localhost:5173',
-    timeout: 120000,
-  },
+  webServer: process.env.CI
+    ? {
+      command: 'pnpm run preview',
+      url: 'http://localhost:4173',
+      timeout: 120000,
+    }
+    : {
+      command: 'pnpm run dev',
+      url: 'http://localhost:5173',
+      reuseExistingServer: true,
+      timeout: 120000,
+    },
 });
