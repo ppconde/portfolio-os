e3ead9c3929f28a850e97ab085fa61c981922939
Pedro Conde <pedro.conde@flutteruki.com>
2026-05-20 14:51:16 +0100

    deps: remove vite-tsconfig-paths from dependencies and update compatibility date in wrangler.jsonc

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,18 @@ import { reactRouter } from '@react-router/dev/vite';
 import { cloudflare } from '@cloudflare/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   build: {
     ssr: true,
     minify: true,
   },
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
     cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tailwindcss(),
     reactRouter(),
-    tsconfigPaths(),
   ],
 });
