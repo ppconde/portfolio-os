925aa77ab086573b533bd3685cb195ff3d2c1af6
Pedro Conde <pedro.conde@flutteruki.com>
2026-08-04 15:46:46 +0100

    Remove future V8 Vite environment API configuration from React Router setup

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,9 +2,6 @@ import type { Config } from '@react-router/dev/config';
 
 export default {
   ssr: true,
-  future: {
-    v8_viteEnvironmentApi: true,
-  },
   async prerender() {
     return ['/home', '/about', '/contact', '/experience'];
   },
